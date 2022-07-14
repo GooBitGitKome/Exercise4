348f5f295f5ce28166423f44cddbf31292ef359a
TakeNap <95421628+GooBitGitKome@users.noreply.github.com>
2022-07-14 14:39:51 +0900

    HoT: initial revision

---
+++ heads_or_tails.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class heads_or_tails{
+    private static int TIMES;
+    heads_or_tails()
+    {
+        TIMES = 3;
+    }
+    public static void main(String[] args){
+        new heads_or_tails().tossCoin(TIMES);
+    }
+
+    public void tossCoin(int times){
+        int cntHead,cntTails;
+        cntHead = cntTails = 0;
+        System.out.println("Tossing a coin...");
+        for(int i = 0 ; i < times; i++)
+        {
+            //0 is head, 1 is tail
+            if((new Random().nextInt() % 2) == 0)
+            {
+                cntHead++;
+                System.out.println(String.format("Round %d: Heads",i));
+            }else{
+                cntTails++;
+                System.out.println(String.format("Round %d: Tails",i));
+            }
+        }
+        System.out.println(String.format("Heads: %d, Tails: %d", cntHead, cntTails));
+    }
+}
